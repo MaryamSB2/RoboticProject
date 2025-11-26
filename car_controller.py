d2548f8c6a965f83fa16f8df31ce671863cc3e69
js964 <138900550+js964@users.noreply.github.com>
2025-11-26 21:23:07 +0000

    updated
    goes back and forth now - need to make sure the distance sensors are in the correct place (should already be)

--- car_controller.py
+++ car_controller.py
@@ -21,6 +21,16 @@ for i in range(4):
     wheels.append(robot.getDevice(wheelsNames[i]))
     wheels[i].setPosition(float('inf'))
     wheels[i].setVelocity(0.0)
+    
+ds = []
+dsNames = ['ds_left', 'ds_right']
+for i in range(2):
+    ds.append(robot.getDevice(dsNames[i]))
+    ds[i].enable(timestep)
+    
+speed = 10.0
+for i in range(4):
+    wheels[i].setVelocity(speed)
 
 # Main loop:
 # - perform simulation steps until Webots is stopping the controller
@@ -28,15 +38,14 @@ while robot.step(timestep) != -1:
     # Read the sensors:
     # Enter here functions to read sensor data, like:
     #  val = ds.getValue()
-    speed = 6.0
 
-    # Process sensor data here.
-    wheels[0].setVelocity(speed)
-    wheels[1].setVelocity(speed)
-    wheels[2].setVelocity(speed)
-    wheels[3].setVelocity(speed)
-    # Enter here functions to send actuator commands, like:
-    #  motor.setPosition(10.0)
-    pass
+    # #Enter here functions to send actuator commands, like:
+     # #motor.setPosition(10.0)
+    if ds[0].getValue() < 1000: #left sensor (towards me)
+        for i in range(4):
+            wheels[i].setVelocity(-10)
+    elif ds[1].getValue() < 1000: # right sensor (away from me)
+        for i in range(4):
+            wheels[i].setVelocity(10)
 
 # Enter here exit cleanup code.
